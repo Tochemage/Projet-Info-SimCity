b59f2555202f7e11b4bb08d0b3641956589bce38
seb_frmg <sebfrmg@users.noreply.github.com>
2022-11-03 12:25:11 +0100

    .

--- Header.h
+++ Header.h
@@ -21,6 +21,23 @@ typedef struct ville
     int mode_jeu; /// 0 communiste, 1 capitaliste
 }t_ville;
 
+typedef struct condition_amelioration
+{
+    int type; /// 0->terrain vague, 1->cabane, 2->maison, 3->immeuble, 4->Gratte-ciel
+    int eau;
+    int electricite;
+    int route;
+}t_amelioration;
+
+typedef struct batiment
+{
+    int type; /// 0->terrain vague(0 hab), 1->cabane(10 hab), 2->maison(50 hab), 3->immeuble(100 hab), 4->Gratte-ciel(1000 hab)
+    char nom_bat[20];
+    int taille;
+    int conso_eau;
+    int conso_elec;
+    int nbr_hab;
+}t_batiment;
 
 
 #endif //V1_HEADER_H
