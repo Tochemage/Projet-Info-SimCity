6aba86da89c896381f851536c1d9b9cadae4156e
Toche <theo.magendie@edu.ece.fr>
2022-11-26 18:50:07 +0100

    commit perso

--- EvoluBat.c
+++ EvoluBat.c
@@ -4,3 +4,20 @@
 
 
 #include "Header.h"
+/*
+void canevolve(t_ville* maVille, t_infos* infos)
+{
+    for(int i=0;i<40;i++)
+    {
+        for(int j=0;j<40;j++)
+        {
+            if(maVille->map[i][j].habitation->type==1 || maVille->map[i][j].habitation->type==2 || maVille->map[i][j].habitation->type==3)
+            {
+                if(maVille->map[i][j].habitation->type==1)
+                {
+                    if(maVille->map[i][j].habitation-> >=infos->I_evolution.Evol_cabane.eau)
+                }
+            }
+        }
+    }
+}*/
