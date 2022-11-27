4999264a2672c7e29f835d634466c0b4a339d004
Clement <clem.larochejoubert@gmail.com>
2022-11-27 01:02:27 +0100

    push sauvegarde terminée

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -86,37 +86,69 @@ void sauvegardeJoueur(t_ville* ville)  //Mettre structure joueur en paramètre
 void chargementJoueur(t_ville* ville)      //Mettre structure joueur en paramètre
 {
     ///Déclarations
-    FILE* fichier=NULL;
+    FILE* fichierVille=NULL;
+    FILE* fichierMap=NULL;
 
     ///Instructions
 
-    //Fichier 1 (structure ville)
-    fichier=fopen("sauvegardestructjoueur.txt", "r"); //Ouverture du fichier
-    if(fichier==NULL) //Si le programme n'arrive pas à ouvrir le fichier
+    ///fichierVille 1 (structure ville)
+    fichierVille=fopen("sauvegardestructjoueur.txt", "r"); //Ouverture du fichierVille
+    if(fichierVille==NULL) //Si le programme n'arrive pas à ouvrir le fichierVille
     {
-        printf("Erreur lors de l'ouverture du fichier de sauvegarde ville\n"); //Message d'erreur d'ouverture
+        printf("Erreur lors de l'ouverture du fichierVille de sauvegarde ville\n"); //Message d'erreur d'ouverture
     }
-    else //Si l'ouverture du fichier a réussi
+    else //Si l'ouverture du fichierVille a réussi
     {
-        fscanf(fichier, "%s\n", ville->nom); //chargement du nom
-        fscanf(fichier, "%ld\n", &ville->argent);
-        fscanf(fichier, "%d\n", &ville->nbr_habitants);
-        fscanf(fichier, "%d\n", &ville->capa_eau);
-        fscanf(fichier, "%d\n", &ville->capa_elec);
-        fscanf(fichier, "%d\n", &ville->impots);
-        fscanf(fichier, "%d\n", &ville->nbr_mois_jeu);
-        fscanf(fichier, "%d\n", &ville->mode_jeu);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_chateauEau);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_centraleElec);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_terrainVague);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_cabane);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_maison);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_immeuble);
-        fscanf(fichier, "%d\n", &ville->possesions.nb_gratteCiel);
+        fscanf(fichierVille, "%s\n", ville->nom); //chargement du nom
+        fscanf(fichierVille, "%ld\n", &ville->argent);
+        fscanf(fichierVille, "%d\n", &ville->nbr_habitants);
+        fscanf(fichierVille, "%d\n", &ville->capa_eau);
+        fscanf(fichierVille, "%d\n", &ville->capa_elec);
+        fscanf(fichierVille, "%d\n", &ville->impots);
+        fscanf(fichierVille, "%d\n", &ville->nbr_mois_jeu);
+        fscanf(fichierVille, "%d\n", &ville->mode_jeu);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_chateauEau);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_centraleElec);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_terrainVague);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_cabane);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_maison);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_immeuble);
+        fscanf(fichierVille, "%d\n", &ville->possesions.nb_gratteCiel);
 
-        fclose(fichier); //Fermeture du fichier
+        fclose(fichierVille); //Fermeture du fichierVille
     }
 
 
-    //Fichier 2 (matrice map)
+    ///Fichier 2 (matrice map)
+    fichierMap=fopen("sauvegardeMap.txt", "r"); //Ouverture du fichierMap
+    if(fichierMap==NULL) //Si le programme n'arrive pas à ouvrir le fichierMap
+    {
+        printf("Erreur lors de l'ouverture du fichierMap de sauvegarde map\n"); //Message d'erreur d'ouverture
+    }
+    else //Si l'ouverture du fichierMap a réussi
+    {
+        for(int i=0; i<NB_LIGNES; i++)      //On va parcourir toutes les cases en commencant par les lignes pour les sauvegarder
+        {
+            for(int j=0; j<NB_COLONNES; j++)
+            {
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].num_case);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].type_case);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].num_color);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].num_case_x);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].num_case_y);
+                fscanf(fichierMap, "%s\n", ville->map[i][j].habitation->nom_bat);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].habitation->num_bat);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].habitation->type);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].habitation->apport_eau);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].habitation->apport_elec);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].habitation->nbr_hab);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].industrie->num_bat);
+                fscanf(fichierMap, "%d\n", &ville->map[i][j].industrie->type);
+                fscanf(fichierMap, "%s\n", ville->map[i][j].industrie->nom_bat);
+
+            }
+        }
+        fclose(fichierMap); //Fermeture du fichier
+    }
+
 }
--- main.c
+++ main.c
@@ -23,7 +23,23 @@ int main() {
     ///Initialisations///
     sp_init_struct(ville, infoVille);   //Sp d'initialisation des structures
 
-    sauvegardeJoueur(ville);
+    //Exemples sauvegarde et chargement
+    //sauvegardeJoueur(ville);
+    //chargementJoueur(ville);
+    /*printf("%d\n", ville->map[0][0].num_case);
+    printf("%d\n", ville->map[0][0].type_case);
+    printf("%d\n", ville->map[0][0].num_color);
+    printf("%d\n", ville->map[0][0].num_case_x);
+    printf("%d\n", ville->map[0][0].num_case_y);
+    printf("%s\n", ville->map[0][0].habitation->nom_bat);
+    printf("%d\n", ville->map[0][0].habitation->num_bat);
+    printf("%d\n", ville->map[0][0].habitation->type);
+    printf("%d\n", ville->map[0][0].habitation->apport_eau);
+    printf("%d\n", ville->map[0][0].habitation->apport_elec);
+    printf("%d\n", ville->map[0][0].habitation->nbr_hab);
+    printf("%d\n", ville->map[0][0].industrie->num_bat);
+    printf("%d\n", ville->map[0][0].industrie->type);
+    printf("%s\n", ville->map[0][0].industrie->nom_bat);*/
 
 
     do{
